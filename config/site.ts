35aae5f7970ebf6556de68ef6c4813c16a3d54c8
kaypappi <wesleyukadike@yahoo.com>
2023-07-17 04:34:14 +0100

    added name and description to site config

--- config/site.ts
+++ config/site.ts
@@ -3,6 +3,12 @@ import type { MainNavItem } from "@/types"
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
+  name: "Viva Jets",
+  description:
+  `Elevate Your Private Flights 
+  to Extraordinary Heights`,
+  /* url: "https://skateshop13.vercel.app/",
+  ogImage: "https://skateshop13.vercel.app/opengraph-image.png", */
   mainNav: [
     {
       title: "Services",
