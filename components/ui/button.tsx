2b4f225868938dda13ce8e951518b4538c2c2bd3
kaypappi <wesleyukadike@yahoo.com>
2023-07-17 18:45:48 +0100

    added animation to buttons on hover

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
+import { motion } from "framer-motion";
 
 import { cn } from "@/lib/utils";
 
@@ -42,7 +45,7 @@ const buttonVariants = cva(
   }
 );
 
-export type ButtonVariantProps = VariantProps<typeof buttonVariants>["variant"]
+export type ButtonVariantProps = VariantProps<typeof buttonVariants>["variant"];
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -54,11 +57,22 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
     return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
+      <motion.div
+      className="w-max h-max"
+      whileHover={{
+        x:[0,5,2,5],
+        y:[0,5,2,5],
+        transition: {
+          duration: 0.5,
+        }
+      }}
+      >
+        <Comp
+          className={cn(buttonVariants({ variant, size, className }))}
+          ref={ref}
+          {...props}
+        />
+      </motion.div>
     );
   }
 );
