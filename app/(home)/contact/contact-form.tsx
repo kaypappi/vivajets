d8a42908ad4403f00bab7dd88b762330abcd847a
kaypappi <wesleyukadike@yahoo.com>
2024-04-15 07:44:43 -0400

    fixed contact form

--- app/(home)/contact/contact-form.tsx
+++ app/(home)/contact/contact-form.tsx
@@ -90,7 +90,6 @@ const ContactForm = () => {
           </label>
           <textarea
             rows={5}
-            aria-rowcount={5}
             name="MMERGE13"
             className=" flex mt-2 mb-8 h-10 w-full rounded-md border border-white/20 bg-transparent px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-400 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-800"
             id="mce-MMERGE13"
