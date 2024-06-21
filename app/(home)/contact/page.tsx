3819ed49320c64195e63f90bb21c7a4d30f34c11
kaypappi <wesleyukadike@yahoo.com>
2024-06-21 10:44:29 -0400

    changed phone number

--- app/(home)/contact/page.tsx
+++ app/(home)/contact/page.tsx
@@ -12,7 +12,7 @@ const contactDetails = [
     icon: () => <Phone className="w-3 h-3" />,
     desc: `Call our customer care representative via the contact number for 
         bookings and enquiries.`,
-    value: " +2347057497533",
+    value: "+23413430614",
   },
   {
     title: "EMAIL ADDRESS",
