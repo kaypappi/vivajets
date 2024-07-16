7d8ad8677bc36f6e978365517f48853ee21e6e63
boluwatifeajayi <bolu4good@gmail.com>
2024-07-16 12:30:11 +0100

    media

--- components/layouts/site-header.tsx
+++ components/layouts/site-header.tsx
@@ -25,7 +25,6 @@ import {
 } from "framer-motion";
 import { ElementRef, useCallback, useEffect, useLayoutEffect, useRef, useState } from "react";
 
-
 import { MainNav } from "@/components/layouts/main-nav";
 import { MobileNav } from "@/components/layouts/mobile-nav";
 import { vi } from "date-fns/locale";
@@ -34,8 +33,8 @@ import { useBreakpoint } from "@/hooks/tailwind";
 let scrollThreshold = [0, 50];
 
 export function SiteHeader() {
-const [showBlur,setShowBlur]=useState(false)
-const isDesktop = useBreakpoint("lg");
+  const [showBlur,setShowBlur]=useState(false)
+  const isDesktop = useBreakpoint("lg");
 
   let { scrollY } = useScroll();
   let scrollYOnDirectionChange = useRef(scrollY.get());
@@ -50,7 +49,6 @@ const isDesktop = useBreakpoint("lg");
     [0.3, isDesktop?0.3:0]
   );
   let backgroundColorTemplate = useMotionTemplate`rgba(250 250 249 / ${backgroundOpacity})`;
-
 
   useMotionValueEvent(scrollY,"change",(latest) => {
     setShowBlur(latest>0)
@@ -83,32 +81,34 @@ const isDesktop = useBreakpoint("lg");
     lastScrollDirection.current = scrollDirection;
   })
 
-  
-
-
   return (
     <motion.header style={{top:isDesktop ? '2.5rem':top }} className="fixed inset-x-0 top-8 lg:top-8 z-40 w-full px-2 ">
-      <motion.div animate={{
-        backgroundColor: `rgba(13,15,43,${showBlur?backgroundOpacity.get():isDesktop?0.3:0})`,
-        backdropFilter: `blur(${showBlur || isDesktop?35:0}px)`,
-      }}
-      transition={{
-        duration:0.5,
-        ease:"easeInOut"
-        
-      }}
-       className="container  w-full px-4 lg:pr-2 flex h-16 items-center  lg:max-w-4xl lg:ml-auto lg:bg-brand lg:backdrop-blur-[35px] lg:bg-opacity-30 rounded-full py-4">
+      <motion.div 
+        animate={{
+          backgroundColor: `rgba(13,15,43,${showBlur?backgroundOpacity.get():isDesktop?0.3:0})`,
+          backdropFilter: `blur(${showBlur || isDesktop?35:0}px)`,
+        }}
+        transition={{
+          duration:0.5,
+          ease:"easeInOut"
+        }}
+        className="container w-full px-4 lg:pr-2 flex h-16 items-center lg:max-w-4xl lg:ml-auto lg:bg-brand lg:backdrop-blur-[35px] lg:bg-opacity-30 rounded-full py-4"
+      >
         <MainNav items={siteConfig.mainNav} />
         <MobileNav
           mainNavItems={siteConfig.mainNav}
           sidebarNavItems={siteConfig.mainNav}
         />
         <div className="lg:flex hidden flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-2">
-            <Button asChild className=" w-max" size={"lg"} variant={"clay"}>
+          <nav className="flex items-center space-x-4">
+            <Link href="/media" className="text-[16px] mr-6 font-medium text-white hover:text-gray-200">
+              Media
+            </Link>
+           
+            <Button asChild className="w-max" size={"lg"} variant={"clay"}>
               <Link href="/contact">
                 <div>
-                +23413430614
+                  +23413430614
                   <span className="sr-only">+23413430614</span>
                 </div>
               </Link>
