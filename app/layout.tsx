3bec02aae4f6367c948ceb1cb7406fdfb3d5c7ca
Boluwatife Ajayi <bolu4good@gmail.com>
2025-09-08 10:33:49 +0100

    redid checker

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,23 +33,11 @@ export default function RootLayout({
             window._linkedin_data_partner_ids.push(_linkedin_partner_id);
           `}
         </Script>
-        <Script id="linkedin-insight-loader" strategy="afterInteractive">
-          {`
-            (function(l) {
-              if (!l){
-                window.lintrk = function(a,b){
-                  window.lintrk.q.push([a,b])
-                };
-                window.lintrk.q=[]
-              }
-              var s = document.getElementsByTagName("script")[0];
-              var b = document.createElement("script");
-              b.type = "text/javascript"; b.async = true;
-              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-              s.parentNode.insertBefore(b, s);
-            })(window.lintrk);
-          `}
-        </Script>
+        <Script
+          id="linkedin-insight-loader"
+          src="https://snap.licdn.com/li.lms-analytics/insight.min.js"
+          strategy="afterInteractive"
+        />
         <noscript>
           <img height="1" width="1" style={{ display: "none" }} alt="" src="https://px.ads.linkedin.com/collect/?pid=6046777&fmt=gif" />
         </noscript>
